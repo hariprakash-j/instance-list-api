7a8761f62cbd8433f41592328b03da22291e8ada
hari <hari@localhost.localdomain>
2021-04-20 02:02:12 +0530

    added comments and doc strings

--- lambda/app.py
+++ lambda/app.py
@@ -13,10 +13,12 @@ class AwsInstances():
         ec2 = boto3.client('ec2')
         response = ec2.describe_regions()
         self.instance_dict = {i['RegionName']:{} for i in response['Regions']}
+
+        # threading makes all the api calls at once or close to togeather so its faster
         self.parallelize2()
 
     def get_instances_in_region(self, region_aws_name):
-        '''gets the instances in a specific region'''
+        '''gets the instances in a specific region and the total number of instances in a region and writes it to the instance_dict'''
         my_config = Config( region_name = region_aws_name)
         ec2_region = boto3.client('ec2', config=my_config)
         response = ec2_region.describe_instances()
@@ -27,6 +29,7 @@ class AwsInstances():
             self.instance_dict[region_aws_name] = {}
     
     def parallelize(self):
+        ''' old method of paralleizing runs all api calls at in parallel '''
         threads = []
         for region in self.instance_dict.keys():
             t = threading.Thread(target = self.get_instances_in_region, args = [region])
@@ -36,10 +39,13 @@ class AwsInstances():
             thread.join()
 
     def parallelize2(self):
+        ''' newer metheod of threading using thread pools runs all api calls at in parallel '''
         with concurrent.futures.ThreadPoolExecutor() as executor:
             results = executor.map(self.get_instances_in_region, self.instance_dict.keys())
 
+# Lambda handler , this is called by the api gateway when there is an event
 def lambda_handler(event, context):
+
     instance_list = AwsInstances()
     return {
         'statusCode': 200,
--- lambda/test_app.py
+++ lambda/test_app.py
@@ -3,10 +3,12 @@ import time
 import tracemalloc
 from app import AwsInstances
 
+# tests the api call , by checking if number of the regions returned by the call is equal or greater than 15
 def test_dict_keys_length():
     instance_list = AwsInstances()
     assert len(instance_list.instance_dict.keys()) >= 15
 
+# checks how long the program takes to run accounting for the overhead in lambda, as our lambda function is limited to 6 seconds
 def test_function_run_duration():
     start = time.perf_counter()
     instance_list = AwsInstances()
@@ -14,6 +16,7 @@ def test_function_run_duration():
     total_time_elaspsed = end - start
     assert total_time_elaspsed <= 4.5
 
+# checks the memory usage of the program accounting for the overhead in lambda, as our lambda function is limited to 200 MB of memory
 def test_function_memory_usage():
     tracemalloc.start()
     instance_list = AwsInstances()
